0757a476272fbb978d5c75a1d50b7ef73fd0fb85
marcocslima <marcocslima@gmail.com>
2026-04-01 13:00:54 +0000

    fix: arquivo dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,22 +1,23 @@
-
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
+{
+  # To learn more about how to use Nix to configure your environment
+  # see: https://developers.google.com/idx/guides/customize-idx-env
+  pkgs, ...
+}: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # Switching to a compatible Node.js version for Vite
-    pkgs.nodejs_20,
-    pkgs.python311Packages.firebase-admin # Adicionado para o script de upload
+    pkgs.nodejs_20
+    # Adicionando o interpretador Python e o pacote firebase-admin
+    pkgs.python311
+    pkgs.python311.pkgs.firebase-admin
   ];
   # Sets environment variables in the workspace
   env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "google.gemini-cli-vscode-companion"
-    ];
+    extensions = ["google.gemini-cli-vscode-companion"];
     # Enable previews
     previews = {
       enable = true;
@@ -30,14 +31,6 @@
     # Workspace lifecycle hooks
     workspace = {
       # Runs when a workspace is first created
-      onCreate = {
-        npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
     };
   };
 }
